7dc5eb01e323bdfa7bd744c41d64d6ce6fe097ba
yinghuali <yinghua.li@uni.lu>
2023-03-22 15:47:43 +0100

    update

--- models/citeseer_gat_train.py
+++ models/citeseer_gat_train.py
@@ -7,33 +7,31 @@ import torch.nn.functional as F
 from torch_geometric.nn import GCNConv
 from sklearn.model_selection import train_test_split
 
-# path_x_np = '../data/citeseer/x_np.pkl'
-# path_edge_index = '../data/citeseer/edge_index_np.pkl'
-# path_y = '../data/citeseer/y_np.pkl'
-# epochs = 10
-# save_model_name = 'citeseer_gat.pt'
-# save_pre_name = 'pre_np_citeseer_gat.pkl'
+path_x_np = '../data/citeseer/x_np.pkl'
+path_edge_index = '../data/citeseer/edge_index_np.pkl'
+path_y = '../data/citeseer/y_np.pkl'
+epochs = 10
+save_model_name = '../target_models/citeseer_gat.pt'
+save_pre_name = '../target_models/pre_np_citeseer_gat.pkl'
 
 # python citeseer_gat_train.py --path_x_np '../data/citeseer/x_np.pkl'  --path_edge_index '../data/citeseer/edge_index_np.pkl' --path_y '../data/citeseer/y_np.pkl' --epochs 10 --save_model_name '../mutation/repeat_models/repeat_1/citeseer_gat.pt' --save_pre_name '../mutation/repeat_models/repeat_1/pre_np_citeseer_gat.pkl'
 
-import argparse
-ap = argparse.ArgumentParser()
-ap.add_argument("--path_x_np", type=str)
-ap.add_argument("--path_edge_index", type=str)
-ap.add_argument("--path_y", type=str)
-ap.add_argument("--epochs", type=int)
-ap.add_argument("--save_model_name", type=str)
-ap.add_argument("--save_pre_name", type=str)
+# import argparse
+# ap = argparse.ArgumentParser()
+# ap.add_argument("--path_x_np", type=str)
+# ap.add_argument("--path_edge_index", type=str)
+# ap.add_argument("--path_y", type=str)
+# ap.add_argument("--epochs", type=int)
+# ap.add_argument("--save_model_name", type=str)
+# ap.add_argument("--save_pre_name", type=str)
 
-
-
-args = ap.parse_args()
-path_x_np = args.path_x_np
-path_edge_index = args.path_edge_index
-path_y = args.path_y
-epochs = args.epochs
-save_model_name = args.save_model_name
-save_pre_name = args.save_pre_name
+# args = ap.parse_args()
+# path_x_np = args.path_x_np
+# path_edge_index = args.path_edge_index
+# path_y = args.path_y
+# epochs = args.epochs
+# save_model_name = args.save_model_name
+# save_pre_name = args.save_pre_name
 
 
 x = pickle.load(open(path_x_np, 'rb'))
@@ -43,13 +41,20 @@ y = pickle.load(open(path_y, 'rb'))
 num_node_features = len(x[0])
 num_classes = len(set(y))
 idx_np = np.array(list(range(len(x))))
-train_idx, test_idx, train_y, test_y = train_test_split(idx_np, y, test_size=0.3, random_state=17)
+train_all_idx, test_idx, train_all_y, test_y = train_test_split(idx_np, y, test_size=0.3, random_state=17)
+
+train_idx, candidate_idx, train_y, candidate_y = train_test_split(train_all_idx, train_all_y, test_size=0.5, random_state=17)
 
 x = torch.from_numpy(x)
 edge_index = torch.from_numpy(edge_index)
 y = torch.from_numpy(y)
 train_y = torch.from_numpy(train_y)
+candidate_y = torch.from_numpy(candidate_y)
 test_y = torch.from_numpy(test_y)
+
+print(len(train_idx))
+print(len(candidate_idx))
+print(len(test_idx))
 
 
 def train(x, edge_index, y):
